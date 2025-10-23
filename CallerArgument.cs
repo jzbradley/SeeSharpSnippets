07c49390f57663bd7cfd0a3da1f120c3c09dea10
J.Z. Bradley <jzbradley@gmail.com>
2025-10-23 16:39:33 -0700

    Update CallerArgument.cs

--- CallerArgument.cs
+++ CallerArgument.cs
@@ -2,57 +2,56 @@ using System.Runtime.CompilerServices;
 
 public static class CallerArgument
 {
-  public static IEnumerable<(T? Value, string? Name)> Parameter<T>(
+  public static IEnumerable<(T? Value, string? Name)> Single<T>(
     this (T? Value, string? Name) first,
     T value,
-    [CallerArgumentExpression("value")] string? paramName = null
+    [CallerArgumentExpression(nameof(value))] string? paramName = null
   )
   {
     yield return first;
-    yield return Parameter(value, paramName);
+    yield return Single(value, paramName);
   }
 
-  public static IEnumerable<(T? Value, string? Name)> Params<T>(
+  public static IEnumerable<(T? Value, string? Name)> Aggregate<T>(
     this (T? Value, string? Name) first,
     T[] parameters,
-    [CallerArgumentExpression("parameters")] string? paramName = null
+    [CallerArgumentExpression(nameof(parameters))] string? paramName = null
   )
   {
     yield return first;
-    foreach (var argument in Params(parameters, paramName))
+    foreach (var argument in Aggregate(parameters, paramName))
       yield return argument;
   }
 
-  public static IEnumerable<(T? Value, string? Name)> Parameter<T>(
+  public static IEnumerable<(T? Value, string? Name)> Single<T>(
     this IEnumerable<(T? Value, string? Name)> arguments,
     T value,
-    [CallerArgumentExpression("value")] string? paramName = null
+    [CallerArgumentExpression(nameof(value))] string? paramName = null
     )
   {
     foreach (var argument in arguments)
       yield return argument;
-    yield return Parameter(value, paramName);
+    yield return Single(value, paramName);
   }
 
-  public static IEnumerable<(T? Value, string? Name)> Params<T>(
+  public static IEnumerable<(T? Value, string? Name)> Aggregate<T>(
     this IEnumerable<(T? Value, string? Name)> arguments,
     T[] parameters,
-    [CallerArgumentExpression("parameters")] string? paramName = null
+    [CallerArgumentExpression(nameof(parameters))] string? paramName = null
     )
-    => arguments.Concat(Params(parameters, paramName));
+    => arguments.Concat(Aggregate(parameters, paramName));
 
-  public static (T? Value, string? Name) Parameter<T>(
+  public static (T? Value, string? Name) Single<T>(
     T value,
-    [CallerArgumentExpression("value")] string? paramName = null
+    [CallerArgumentExpression(nameof(value))] string? paramName = null
     )
     => (value, paramName);
 
-  public static IEnumerable<(T? Value, string? Name)> Params<T>(T[] parameters,
-    [CallerArgumentExpression("parameters")]
+  public static IEnumerable<(T? Value, string? Name)> Aggregate<T>(T[] parameters,
+    [CallerArgumentExpression(nameof(parameters))]
     string? paramName = null
     )
     => paramName == null 
       ? parameters.Select((value, i) => ((T?)value, $"[{i}]"))
       : parameters.Select((value, i) => ((T?)value, $"{paramName}[{i}]"));
-
 }
